b3704d3e67d75e6008949312f6594128f9fd90c3
MariaMazaeva <mazaeva.mar@gmail.com>
2026-09-13 11:15:43 +0300

    added comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,16 @@ import Results from './components/Results';
  
 function App() {
   // API called when the user submits a search query
-  // const handleSearch = (query) => {
-  //   console.log('Searching for:', query);
-  // }; 
+  const handleSearch = (query) => {
+    console.log('Searching for:', query);
+  }; 
 
 
    return (
     <div className="flex flex-col items-start p-0 w-full min-h-screen
             bg-[#F4F1EC] flex-none 
             order-none self-stretch grow-0">
-      
+      {/* <SearchBox onSearch={handleSearch} /> */}
       <Results/>
     </div>
   );
