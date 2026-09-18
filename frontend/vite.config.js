52676c1e8d7e0dd03ab66ee436ae4dd82461ba04
MariaMazaeva <mazaeva.mar@gmail.com>
2026-09-18 13:23:03 +0300

    added dependences

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/WB_project_Metropolia/",
   plugins: [react(), tailwindcss()],
-})
+}) 
   
